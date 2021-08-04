a15d14d7111853426705281dfca6299533dfcfc8
sandeep332001 <sandeep030301@gmail.com>
2021-08-04 10:39:13 +0530

    Start 4th Aug

---
+++ hello.cpp
@@ -0,0 +1,22 @@
+/* Sandeep Code's Only */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t;
+	cin >> t;
+	cin.ignore();
+	while (t--)
+	{
+		string s;
+		getline(cin, s);
+		cout << s << endl;
+	}
+	return 0;
+}
